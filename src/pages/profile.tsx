36aa9706174ac1e99995f0ae855e3c45a1c04e29
Ayata3 <ayata.grobserver@gmail.com>
2020-05-04 20:50:30 +0900

    [feat] add image component

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -3,11 +3,7 @@ import React from 'react'
 import ProfileTemplate from '../components/templates/ProfileTemplate'
 
 const ProfilePage: React.FC = () => {
-  return (
-    <ProfileTemplate>
-      <p>This is the profile page.</p>
-    </ProfileTemplate>
-  )
+  return <ProfileTemplate />
 }
 
 export default ProfilePage
