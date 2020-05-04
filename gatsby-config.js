b00e09988ffadb67fa48284f7c975b55b272185a
Ayata3 <ayata.grobserver@gmail.com>
2020-05-04 17:56:40 +0900

    [feat] add fetching posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,14 +21,32 @@ module.exports = {
         purgeOnly: [],
       },
     },
-    // {
-    //   resolve: 'gatsby-source-filesystem',
-    //   options: {
-    //     name: 'images',
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
-    // 'gatsby-plugin-sharp',
-    // 'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'images',
+        path: `${__dirname}/src/images/`,
+        ignore: ['**/.*'], // ignore files starting with a dot
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+          },
+        ],
+      },
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
   ],
 }
---
+++ src/queries/posts/index.tsx
@@ -0,0 +1,1 @@
+export { FETCH_POSTS } from './search'
